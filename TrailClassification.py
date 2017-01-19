db72b198d43f8258953cfbd70d14f632e1c63dac
chrissims712 <chrissims712@gmail.com>
2017-01-19 17:19:11 -0600

    Getting closer.

---
+++ TrailClassification.py
@@ -0,0 +1,46 @@
+def TrailMoto(x):
+	if x = 0:
+		return 'Not indicated'
+	elif x = 5:
+		return 'Open to all vehicles, Yearlong'
+	elif x = 6:
+		return 'Open to all vehicles, Seasonal'
+	elif x = 7:
+		return 'Open to vehicles 50" or less width, Yearlong'
+	elif x = 9:
+		return 'Open to motorcycles, Yearlong'
+	elif x = 10:
+		return 'Open to motorcycles, Seasonal'
+	elif x = 11:
+		return 'Special Designation, Yearlong'
+	elif x = 12:
+		return 'Special Designation, Seasonal'
+	elif x = 16:
+		return 'Open to vehicles 50" or greater width, Yearlong'
+	elif x = 17:
+		return 'Open to vehicles 50" or greater width, Seasonal'
+		
+		
+def TrailMoto(x):
+     if x == 0:
+          return 'Not indicated'
+     elif x == 5:
+          return 'Open to all vehicles, Yearlong'
+     elif x == 6:
+          return 'Open to all vehicles, Seasonal'
+     elif x == 7:
+          return 'Open to vehicles 50" or less width, Yearlong'
+	 elif x == 8:
+		  return 'Open to vehicles 50" or less width, Seasonal'
+     elif x == 9:
+          return 'Open to motorcycles, Yearlong'
+     elif x == 10:
+          return 'Open to motorcycles, Seasonal'
+     elif x == 11:
+          return 'Special Designation, Yearlong'
+     elif x == 12:
+          return 'Special Designation, Seasonal'
+     elif x == 16:
+          return 'Open to vehicles 50" or greater width, Yearlong'
+     elif x == 17:
+          return 'Open to vehicles 50" or greater width, Seasonal'
